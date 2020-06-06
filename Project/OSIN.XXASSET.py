9f5a36a3648fb64722f92ff7d90ae18e0a66480d
kgagan90 <gagan.kumar@arcadisgen.com>
2020-06-06 12:23:21 +0100

    New OS script

---
+++ Project/OSIN.XXASSET.py
@@ -0,0 +1,6 @@
+def beforeCreateMboSet(ctx): 
+   struc = ctx.getData()
+   siteid = struc.getCurrentData("SITEID")
+   if siteid is None or siteid =="":
+       struc.setCurrentData("SITEID", "BEDFORD") 
+       struc.setCurrentData("ORGID", "EAGLENA")
